7c956d4a5bc2dd4426a52285921485ca78edf382
Matias DEVEZE <matiasdeveze@gmail.com>
2025-01-20 17:16:48 +0100

    feat: add footer

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Home from "./components/home/Home";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <div>
-      <Home />
+        <Home />
+        <Footer />
     </div>
   );
 }
