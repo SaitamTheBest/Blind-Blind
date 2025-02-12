c51721bd8238c5120af9644fb47295a441e0dd7b
LaCrocs <enzolacroix23@gmail.com>
2025-02-12 14:46:56 +0100

    feat : animation answers table

--- front/src/components/games/TableBody.tsx
+++ front/src/components/games/TableBody.tsx
@@ -23,22 +23,38 @@ const TableBody: React.FC<TableBodyProps> = ({ guess, randomItem }) => {
 
     return (
         <tbody>
-            {guess.map((message, index) => (
-                <tr key={index}>
-                    <td className={message.isCorrect.artists}>{(message.artists).join(', ')}</td>
-                    <td className={message.isCorrect.album}>{message.album}</td>
-                    <td className={message.isCorrect.genres}>{(message.genres).join(', ')}</td>
-                    <td className={`${message.isCorrect.popularity} ${getArrowClass(message.popularity, randomItem.popularity)}`}>
-                        {message.popularity}
-                    </td>
-                    <td className={`${message.isCorrect.release_date} ${getArrowClass(parseInt(message.release_date.split('-')[0]), parseInt(randomItem.release_date.split('-')[0]))}`}>
-                        {message.release_date.split('-')[0]}
-                    </td>
-                    <td className={message.isCorrect.name}>{message.name}</td>
+        {guess.map((message, rowIndex) => {
+            const isNewRow = rowIndex === 0; // Toujours la première ligne du tableau (nouvelle réponse ajoutée)
+
+            return (
+                <tr key={`${message.name}-${rowIndex}`} className={isNewRow ? 'new-row' : ''}>
+                    {[
+                        { value: message.artists.join(', '), className: message.isCorrect.artists },
+                        { value: message.album, className: message.isCorrect.album },
+                        { value: message.genres.join(', '), className: message.isCorrect.genres },
+                        {
+                            value: message.popularity,
+                            className: `${message.isCorrect.popularity} ${getArrowClass(message.popularity, randomItem.popularity)}`
+                        },
+                        {
+                            value: message.release_date.split('-')[0],
+                            className: `${message.isCorrect.release_date} ${getArrowClass(parseInt(message.release_date.split('-')[0]), parseInt(randomItem.release_date.split('-')[0]))}`
+                        },
+                        { value: message.name, className: message.isCorrect.name }
+                    ].map((cell, colIndex) => (
+                        <td
+                            key={`${message.name}-${colIndex}`} // Forcer React à re-render chaque cellule
+                            className={cell.className}
+                            style={isNewRow ? { animationDelay: `${colIndex * 0.4}s` } : { opacity: 1, transform: "translateX(0)" }}
+                        >
+                            {cell.value}
+                        </td>
+                    ))}
                 </tr>
-            ))}
+            );
+        })}
         </tbody>
     );
-}
+};
 
 export default TableBody;
