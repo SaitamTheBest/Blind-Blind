7432e339567aaa6530859421bd18b49a17ba58bf
Matias DEVEZE <matiasdeveze@gmail.com>
2025-04-09 19:04:20 +0200

    fix: update env

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,9 @@ const app = express();
 export const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: `${process.env.FRONTEND_URL}`,
+}));
 app.use(express.json());
 
 // Routes
