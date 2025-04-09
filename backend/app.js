62608c709a17feae98eb7dbe39020bf66165f63c
Matias DEVEZE <matiasdeveze@gmail.com>
2025-04-09 19:25:53 +0200

    test: test

--- backend/app.js
+++ backend/app.js
@@ -10,9 +10,7 @@ const app = express();
 export const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({
-    origin: `${process.env.FRONTEND_URL}`,
-}));
+app.use(cors());
 app.use(express.json());
 
 // Routes
