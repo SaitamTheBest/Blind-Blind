ef07f9646810a894dc794fb5d5f77476fae16253
LacroixEnzo <168363125+LaCrocs@users.noreply.github.com>
2025-06-23 23:23:28 +0200

    fix : bug song duplication with artists

--- backend/services/getAllTracks.js
+++ backend/services/getAllTracks.js
@@ -6,28 +6,41 @@ async function getAllTracks() {
         connection = await getDbConnection();
 
         const [rows] = await connection.execute(`
-        SELECT
-            t.id AS track_id,
-            t.name,
-            t.album,
-            t.release_year,
-            t.spotify_url,
-            t.preview_url,
-            t.image_url AS image,
-            t.popularity,
-            a.followers,
-            a.image_url,
-            GROUP_CONCAT(a.Nationality) as nationality,
-            t.performer_type,
-            GROUP_CONCAT(a.name) AS artists,
-            GROUP_CONCAT(a.genres) AS genres
-        FROM tracks t
-            LEFT JOIN track_artists ta ON t.id = ta.track_id
-            LEFT JOIN artists a ON ta.artist_id = a.id
-        GROUP BY t.id, t.name, t.album, t.release_year, t.spotify_url, t.preview_url, t.image_url, t.popularity, a.followers, a.image_url, t.performer_type
-        ORDER BY t.popularity DESC;
-
-    `);
+            SELECT
+                t.id AS track_id,
+                t.name,
+                t.album,
+                t.release_year,
+                t.spotify_url,
+                t.preview_url,
+                t.image_url AS image,
+                t.popularity,
+                t.performer_type,
+                (
+                    SELECT a2.followers
+                    FROM track_artists ta2
+                             JOIN artists a2 ON ta2.artist_id = a2.id
+                    WHERE ta2.track_id = t.id
+                    ORDER BY ta2.artist_id
+                    LIMIT 1
+                ) AS followers,
+                (
+                    SELECT a3.image_url
+                    FROM track_artists ta3
+                    JOIN artists a3 ON ta3.artist_id = a3.id
+                    WHERE ta3.track_id = t.id AND a3.image_url IS NOT NULL AND a3.image_url != ''
+                    ORDER BY ta3.artist_id
+                    LIMIT 1
+                ) AS image_url,
+                GROUP_CONCAT(DISTINCT a.name) AS artists,
+                GROUP_CONCAT(DISTINCT a.genres) AS genres,
+                GROUP_CONCAT(DISTINCT a.Nationality) AS nationality
+            FROM tracks t
+                LEFT JOIN track_artists ta ON t.id = ta.track_id
+                LEFT JOIN artists a ON ta.artist_id = a.id
+            GROUP BY t.id, t.name, t.album, t.release_year, t.spotify_url, t.preview_url, t.image_url, t.popularity, t.performer_type
+            ORDER BY t.popularity DESC;
+        `);
 
         return rows.map(track => {
             const artists = track.artists?.split(',').map(a => a.trim()) || [];
